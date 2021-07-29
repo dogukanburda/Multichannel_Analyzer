0d4558d2de90b1f11bf3eb6ebe01ae4c09597b96
Dogukan <dogukankefeli@gmail.com>
2021-07-29 18:30:08 +0300

    removed rtc for now

--- Multichannel_Analyzer.ino
+++ Multichannel_Analyzer.ino
@@ -33,7 +33,7 @@
 #include <Adafruit_GFX.h>    // Core graphics library
 #include <Adafruit_ST7735.h> // Hardware-specific library for ST7735
 #include <SPI.h>
-#include <RTClib.h>            // include Adafruit RTC library
+//#include <RTClib.h>            // include Adafruit RTC library
 
 // Defining SPI Pins for TFT LCD
 #define TFT_CS        53
@@ -43,10 +43,10 @@
 
 Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_RST);
 // Initialize RTC module and configure the days
-RTC_DS3231 rtc;
-DateTime   now;
-char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
-char dow_matrix[7][10] = {"SUNDAY", "MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY", "SATURDAY"};
+//RTC_DS3231 rtc;
+//DateTime   now;
+//char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+//char dow_matrix[7][10] = {"SUNDAY", "MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY", "SATURDAY"};
 
 
 char _buffer[11];
@@ -55,41 +55,40 @@ int pulslar[128]; // For drawing on LCD
 int original_log[128];    // For keeping the original logs of pulses
 int toplam_puls;          // To display the number of pulses occurred in the region of interest (ROI)
 float faktor=1;         // Multiplying factor for the histogram to not exceed the screen
-
+int deger;
 unsigned long oncekizaman = 0; 
 
-byte x_pos[7] = {8, 8, 6, 4, 4, 8, 4}; // 29,29,23,11,17,29,17
-static byte previous_dow = 8;
+//byte x_pos[7] = {8, 8, 6, 4, 4, 8, 4}; // 29,29,23,11,17,29,17
+//static byte previous_dow = 8;
 
  
 
 void setup(void) {
-  Serial.begin(9600);
+  //Serial.begin(9600);
   tft.initR(INITR_BLACKTAB);
-  rtc.begin();
-  rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+  //rtc.begin();
+  //rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
   //pinMode(2, INPUT);
   pinMode(2, INPUT); // comparator çıkışı pin 2
-  pinMode(3, OUTPUT);  //reset pulse'ı pin 3
+  pinMode(4, OUTPUT);  //reset pulse'ı pin 3
 
   int tolam_puls = 0;
   for (int i = 0; i < 128; i++)
        {
         pulslar[i] = 0;
-       }
-  for (int i = 0; i < 128; i++)
-       {
         original_log[i] = 0;
        }
-  // Use this initializer if using a 1.8" TFT screen:
-  tft.initR(INITR_BLACKTAB);      // Init ST7735S chip, black tab
+  
+  //tft.initR(INITR_BLACKTAB);      // Init ST7735S chip, black tab
   tft.fillScreen(ST77XX_BLACK);
-  tft.fillScreen(ST77XX_BLACK);
+  //tft.fillScreen(ST77XX_BLACK);
   // Drawing the outline of sections for the display
-  tft.drawFastHLine(0, 130, 63, ST7735_BLUE);
-  tft.drawFastHLine(0, 101, tft.width(), ST7735_BLUE);
-  tft.drawFastVLine(64, 100, 59, ST7735_BLUE);
-  tft.drawFastHLine(0, 100,  tft.width(), ST7735_BLUE);
+  //tft.drawFastHLine(0, 130, 63, ST7735_YELLOW);
+  //tft.drawLine(1,160,1,1,ST7735_YELLOW);
+  tft.drawFastHLine(1, 1, tft.width(), ST7735_YELLOW);
+  tft.drawFastHLine(0, 101, tft.width(), ST7735_YELLOW);
+  tft.drawFastVLine(64, 100, 59, ST7735_YELLOW);
+  //tft.drawFastHLine(0, 100,  tft.width(), ST7735_YELLOW);
   tft.setTextColor(ST7735_WHITE, ST7735_BLACK);     // set text color to white and black background
   tft.setTextSize(1);                               // text size = 1
   //Outline for dividers' value and toplam puls
@@ -108,30 +107,31 @@ void loop() {
   unsigned long ilkzaman = millis();
   int val = digitalRead(2);
   if (val == 1) {
-    int deger = analogRead(A5);   // genliği okuduğum pin A5
-    digitalWrite(3,HIGH);
-    delayMicroseconds(1);
-    digitalWrite(3,LOW);
+    deger = analogRead(A5);   // genliği okuduğum pin A5
+    //deger=deger/1.6321;
+    digitalWrite(4,HIGH);
+    delayMicroseconds(100);
+    digitalWrite(4,LOW);
 
   Serial.println(deger);
   int toplam_puls = 0;
-  now = rtc.now();  // read current time and date from the RTC chip
-  tft.setCursor(x_pos[previous_dow], 103);
-  tft.setTextColor(ST7735_CYAN, ST7735_BLACK);     // set text color to cyan and black background
-  tft.print( dow_matrix[now.dayOfTheWeek()] );
+//  now = rtc.now();  // read current time and date from the RTC chip
+//  tft.setCursor(x_pos[previous_dow], 103);
+//  tft.setTextColor(ST7735_CYAN, ST7735_BLACK);     // set text color to cyan and black background
+//  tft.print( dow_matrix[now.dayOfTheWeek()] );
   
  
   // print date
-  sprintf( _buffer, "%02u-%02u-%04u", now.day(), now.month(), now.year() );
-  tft.setCursor(4, 112);
-  tft.setTextColor(ST7735_YELLOW, ST7735_BLACK);     // set text color to yellow and black background
-  tft.print(_buffer);
-  
-  // print time
-  sprintf( _buffer, "%02u:%02u:%02u", now.hour(), now.minute(), now.second() );
-  tft.setCursor(4, 121);
-  tft.setTextColor(ST7735_GREEN, ST7735_BLACK);     // set text color to green and black background
-  tft.print(_buffer);
+//  sprintf( _buffer, "%02u-%02u-%04u", now.day(), now.month(), now.year() );
+//  tft.setCursor(4, 112);
+//  tft.setTextColor(ST7735_, ST7735_BLACK);     // set text color to yellow and black background
+//  tft.print(_buffer);
+//  
+//  // print time
+//  sprintf( _buffer, "%02u:%02u:%02u", now.hour(), now.minute(), now.second() );
+//  tft.setCursor(4, 121);
+//  tft.setTextColor(ST7735_GREEN, ST7735_BLACK);     // set text color to green and black background
+//  tft.print(_buffer);
 
   int Index = map(deger,0,1023,0,127);
   original_log[Index] = original_log[Index] + 1;    // Reads from analog pin and appends to the right channel accordingly
@@ -152,8 +152,6 @@ void loop() {
   for (int i = 0; i < 128; i++){
     tft.drawLine(i,100,i,100-pulslar[i],ST7735_RED);
     tft.drawLine(i,100-int(faktor*pulslar[i]),i,0,ST7735_BLACK);
-    //tft.drawLine(div0,100,div0,0,ST7735_WHITE);
-    //tft.drawLine(div1,100,div1,0,ST7735_WHITE);
     }
 
   
@@ -178,16 +176,16 @@ void loop() {
   tft.drawLine(div0,100,div0,0,ST7735_WHITE);
 
 
-  tft.setCursor(23,132);
+  tft.setCursor(26,132);
   tft.print(div0);
   tft.print("   ");
-  tft.setCursor(23,141);
+  tft.setCursor(26,141);
   tft.print(div1);
   tft.print("   ");
 
   tft.setCursor(35,150);
   tft.print(toplam_puls);
-  tft.print("  ");
+  tft.print(" ");
   unsigned long ikincizaman = millis();
   unsigned long timer=ikincizaman-ilkzaman;
   Serial.println(timer);
@@ -195,64 +193,63 @@ void loop() {
 }
 
 
-void RTC_display()
-{
-  char _buffer[11];
-  char dow_matrix[7][10] = {"SUNDAY", "MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY", "SATURDAY"};
-  byte x_pos[7] = {8, 8, 6, 4, 4, 8, 4}; // 29,29,23,11,17,29,17
-  static byte previous_dow = 8;
- 
-  // print day of the week
-  if( previous_dow != now.dayOfTheWeek() )
-  {
-    previous_dow = now.dayOfTheWeek();
-//    tft.fillRect(11, 55, 108, 14, ST7735_BLACK);     // draw rectangle (erase day from the display)
-    tft.setCursor(x_pos[previous_dow], 103);
-    tft.setTextColor(ST7735_CYAN, ST7735_BLACK);     // set text color to cyan and black background
-    tft.print( dow_matrix[now.dayOfTheWeek()] );
-  }
- 
-  // print date
-  sprintf( _buffer, "%02u-%02u-%04u", now.day(), now.month(), now.year() );
-  tft.setCursor(4, 112);
-  tft.setTextColor(ST7735_YELLOW, ST7735_BLACK);     // set text color to yellow and black background
-  tft.print(_buffer);
-  
-  // print time
-  sprintf( _buffer, "%02u:%02u:%02u", now.hour(), now.minute(), now.second() );
-  tft.setCursor(4, 121);
-  tft.setTextColor(ST7735_GREEN, ST7735_BLACK);     // set text color to green and black background
-  tft.print(_buffer);
-}
-void testfastlines(uint16_t color1, uint16_t color2) {
-  tft.fillScreen(ST77XX_BLACK);
-  for (int16_t y=0; y < tft.height(); y+=5) {
-    tft.drawFastHLine(0, y, tft.width(), color1);
-  }
-  for (int16_t x=0; x < tft.width(); x+=5) {
-    tft.drawFastVLine(x, 0, tft.height(), color2);
-  }
-}
-
-void testdrawrects(uint16_t color) {
-  tft.fillScreen(ST77XX_BLACK);
-  for (int16_t x=0; x < tft.width(); x+=6) {
-    tft.drawRect(tft.width()/2 -x/2, tft.height()/2 -x/2 , x, x, color);
-  }
-}
-
-void testfillrects(uint16_t color1, uint16_t color2) {
-  tft.fillScreen(ST77XX_BLACK);
-  for (int16_t x=tft.width()-1; x > 6; x-=6) {
-    tft.fillRect(tft.width()/2 -x/2, tft.height()/2 -x/2 , x, x, color1);
-    tft.drawRect(tft.width()/2 -x/2, tft.height()/2 -x/2 , x, x, color2);
-  }
-}
-
-void testfillcircles(uint8_t radius, uint16_t color) {
-  for (int16_t x=radius; x < tft.width(); x+=radius*2) {
-    for (int16_t y=radius; y < tft.height(); y+=radius*2) {
-      tft.fillCircle(x, y, radius, color);
-    }
-  }
-}
+//void RTC_display()
+//{
+//  char _buffer[11];
+//  char dow_matrix[7][10] = {"SUNDAY", "MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY", "SATURDAY"};
+//  byte x_pos[7] = {8, 8, 6, 4, 4, 8, 4}; // 29,29,23,11,17,29,17
+//  static byte previous_dow = 8;
+// 
+//  // print day of the week
+//  if( previous_dow != now.dayOfTheWeek() )
+//  {
+//    previous_dow = now.dayOfTheWeek();
+////    tft.fillRect(11, 55, 108, 14, ST7735_BLACK);     // draw rectangle (erase day from the display)
+//    tft.setCursor(x_pos[previous_dow], 103);
+//    tft.setTextColor(ST7735_CYAN, ST7735_BLACK);     // set text color to cyan and black background
+//    tft.print( dow_matrix[now.dayOfTheWeek()] );
+//  }
+// 
+//  // print date
+//  sprintf( _buffer, "%02u-%02u-%04u", now.day(), now.month(), now.year() );
+//  tft.setCursor(4, 112);
+//  tft.setTextColor(ST7735_YELLOW, ST7735_BLACK);     // set text color to yellow and black background
+//  tft.print(_buffer);
+//  
+//  // print time
+//  sprintf( _buffer, "%02u:%02u:%02u", now.hour(), now.minute(), now.second() );
+//  tft.setCursor(4, 121);
+//  tft.setTextColor(ST7735_GREEN, ST7735_BLACK);     // set text color to green and black background
+//  tft.print(_buffer);
+//}
+//void testfastlines(uint16_t color1, uint16_t color2) {
+//  tft.fillScreen(ST77XX_BLACK);
+//  for (int16_t y=0; y < tft.height(); y+=5) {
+//    tft.drawFastHLine(0, y, tft.width(), color1);
+//  }
+//  for (int16_t x=0; x < tft.width(); x+=5) {
+//    tft.drawFastVLine(x, 0, tft.height(), color2);
+//  }
+//}
+//
+//void testdrawrects(uint16_t color) {
+//  tft.fillScreen(ST77XX_BLACK);
+//  for (int16_t x=0; x < tft.width(); x+=6) {
+//    tft.drawRect(tft.width()/2 -x/2, tft.height()/2 -x/2 , x, x, color);
+//  }
+//}
+//
+//void testfillrects(uint16_t color1, uint16_t color2) {
+//  tft.fillScreen(ST77XX_BLACK);
+//  for (int16_t x=tft.width()-1; x > 6; x-=6) {
+//    tft.fillRect(tft.width()/2 -x/2, tft.height()/2 -x/2 , x, x, color1);
+//    tft.drawRect(tft.width()/2 -x/2, tft.height()/2 -x/2 , x, x, color2);
+//  }
+//}
+//
+//void testfillcircles(uint8_t radius, uint16_t color) {
+//  for (int16_t x=radius; x < tft.width(); x+=radius*2) {
+//    for (int16_t y=radius; y < tft.height(); y+=radius*2) {
+//      tft.fillCircle(x, y, radius, color);
+//    }
+//  }
